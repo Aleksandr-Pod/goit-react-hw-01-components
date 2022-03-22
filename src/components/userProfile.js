51edad8291fba67a6313b6b72749e328632ff877
Aleksandr-Pod <stdfire@gmail.com>
2022-03-22 11:03:26 +0200

    final

--- src/components/userProfile.js
+++ src/components/userProfile.js
@@ -1,35 +1,36 @@
 import PropTypes from 'prop-types'
+import styles from './profile.module.css'
 
 export default function Profile({ username, tag, location, avatar, stats }) {
-    return <div>
-        <div class="profile">
-        <div class="description">
-            <img
-            src={avatar}
-            alt="User avatar"
-            class="avatar"
-            />
-            <p class="name">{username}</p>
-            <p class="tag">{tag}</p>
-            <p class="location">{location}</p>
-        </div>
+    return (
+        <div class={styles.profile}>
+            <div class={styles.description}>
+                <img
+                src={avatar}
+                alt="User avatar"
+                class={styles.avatar}
+                />
+                <p class={styles.name}>{username}</p>
+                <p class={styles.tag}>{tag}</p>
+                <p class={styles.location}>{location}</p>
+            </div>
 
-        <ul class="stats">
-            <li>
-            <span class="label">Followers</span>
-            <span class="quantity">{stats.followers}</span>
-            </li>
-            <li>
-            <span class="label">Views</span>
-            <span class="quantity">{stats.views}</span>
-            </li>
-            <li>
-            <span class="label">Likes</span>
-            <span class="quantity">{stats.likes}</span>
-            </li>
-        </ul>
+            <ul class={styles.stats}>
+                <li>
+                    <span class={styles.label}>Followers</span>
+                    <span class={styles.quantity}>{stats.followers}</span>
+                </li>
+                <li>
+                    <span class={styles.label}>Views</span>
+                    <span class={styles.quantity}>{stats.views}</span>
+                </li>
+                <li>
+                    <span class={styles.label}>Likes</span>
+                    <span class={styles.quantity}>{stats.likes}</span>
+                </li>
+            </ul>
         </div>
-    </div>;
+    )
 } 
 
 Profile.propTypes = {
